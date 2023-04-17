4478afddbd8d2b9abdd1d996814f9aaf4c0441db
edseldim <edselmanuel@hotmail.com>
2023-04-16 23:45:59 -0500

    created etl main script for data extraction

---
+++ Scripts/etl_main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import logging
+import json
+
+from extract import get_list_reference, get_price_data, checkpoint_data_extraction
+from load import save_data_checkpoint
+from transform import obtain_usd_rates, retrieve_steam_data, process_country_prices
+
+def set_up_logger():
+    logger = logging.getLogger("etl_main")
+    handler1 = logging.FileHandler('Scripts/Logs/logger1.log')
+    formatter1 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler1.setFormatter(formatter1)
+    logger.addHandler(handler1)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+        logger.addHandler(handler1)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+def main():
+    logger = set_up_logger()
+    videogame_urls_processed = get_list_reference()
+    price_data = process_country_prices(get_price_data())
+    save_data_checkpoint("usd_rates_reference.txt", price_data)
+    save_data_checkpoint("videogames_id_reference.txt", videogame_urls_processed)
+    usd_rates = obtain_usd_rates(price_data)
+    checkpoint_game_id, checkpoint_index = checkpoint_data_extraction()
+    game_data = retrieve_steam_data(videogame_urls_processed, usd_rates, checkpoint_game_id, checkpoint_index)
+    steam_prices_df = pd.read_json(json.dumps(game_data)).T
+    steam_prices_df.to_excel("Data/list_1_processed_again.xlsx")
+
+if __name__ == "__main__":
+    main()
